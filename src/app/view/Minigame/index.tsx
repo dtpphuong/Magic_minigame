127a7dc88c24e85c8e77c238b87eeeba80e52dfa
phuong <duongthanhphuong7413@gmail.com>
2023-10-15 14:07:04 +0700

    update

--- src/app/view/Minigame/index.tsx
+++ src/app/view/Minigame/index.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React from "react";
 import { styled } from "styled-components";
 import images from '../../../../public/images/index.js'
 import question from "./config.js";
@@ -90,30 +90,73 @@ const Btn = styled.button`
         opacity: 0.6;
     }
 `
+const StyledInvalid = styled.div`
+    width: 100%;
+    height: auto;
+    display: flex;
+    flex-direction: column;
+    align-items: center
+    justify-content: center;
+    text-align: center;
+    font-size: 12px;
+    line-height: 24px;
+    font-weight: 600;
+    color: #ffa24e;
+`
+const ResetText = styled.p`
+    font-size: 14px;
+    line-height: 24px;
+    color: #fff;
+    font-weight: 600;
+    cursor: pointer;
+    &:hover {
+        txt-decoration: underline;
+        color: #ccc;
+    }
+`
 
 const MiniGame = () => {
     const [questionValue, setQuestionValue] = React.useState('')
-    const [changeValue, setChangeValue] = React.useState(false)
+    const [Invalid, setSetInvalid] = React.useState<boolean>(false)
+    const [changeValue, setChangeValue] = React.useState<boolean>(false)
+    const [listIdData, setListIdData] = React.useState<any>([])
 
     const handleRandom = () => {
         const usedNumbers: number[] = [];
-        // if (usedNumbers.length === question.length) {
-        //     setQuestionValue("Đã sử dụng tất cả các số.")
-        //     return;
-        // }
+        if (listIdData.length === question.length) {
+            setSetInvalid(true)
+            return;
+        }
         const min: number = 1;
         const max: number = 50;
         let randomNumber: number;
         do {
             randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
         } while (usedNumbers.includes(randomNumber));
-
         usedNumbers.push(randomNumber);
-        
         const val = question.find((e) => e.id === usedNumbers[0])
+        const a = [...listIdData, val?.id]
+        setListIdData(a)
+        localStorage.setItem('listIdQuestion', JSON.stringify(a))
         setQuestionValue(String(val?.content))
         setChangeValue(true)
     };
+    
+    const handledReset = () => {
+        window.localStorage.removeItem("listIdQuestion");
+        handleRandom()
+    }
+
+    React.useEffect(() => {
+        const item = localStorage.getItem('listIdQuestion');
+        if (item) {
+            const parsedList = JSON.parse(item);
+            setListIdData(parsedList);
+        }
+    }, []);
+
+    console.log(listIdData);
+
 
     return (
         <Wrapper>
@@ -123,14 +166,20 @@ const MiniGame = () => {
                         <StyleCardEnd>
                             {questionValue}
                         </StyleCardEnd>
-                    ): (
+                    ) : (
                         <Image className="shaked" src={images.card} alt="card" />
                     )}
                 </div>
                 {!changeValue ? (
                     <Btn onClick={() => handleRandom()}>Start</Btn>
-                ):(
+                ) : (
                     <Btn onClick={() => setChangeValue(!changeValue)}>End</Btn>
+                )}
+                {Invalid && (
+                    <StyledInvalid>
+                    {Invalid && "Đã sử dụng tất cả các số."}
+                    <ResetText onClick={() => handledReset()}>Resetting</ResetText>
+                </StyledInvalid>
                 )}
             </Container>
         </Wrapper>
